27af90e1b70346a36d15aafa1c9e55d6d6c93c01
Jacques Kvam <jacques@verdigris.co>
2016-10-30 12:22:42 -0700

    update

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+from keras.models import Model
 from keras.layers import Input, Reshape
 from keras.layers.convolutional import Convolution2D
 from attention import Attention
@@ -9,7 +10,6 @@ from attention import Attention
 IMAGE_SIZE = 28
 N_DIGITS = 3
 
-inputs = Input(shape=(1, IMAGE_SIZE, N_DIGITS * IMAGE_SIZE))
 
 def convolutional_features(inp, filters, size):
     conv = Convolution2D(filters, size, size, border_mode='same')(inp)
@@ -24,12 +24,22 @@ def cost():
 FILTERS = 16
 FILTER_SIZE = 3
 
-cfeatures = convolutional_features(inputs, FILTERS, FILTER_SIZE)
+def model():
+    inputs = Input(shape=(1, IMAGE_SIZE, N_DIGITS * IMAGE_SIZE))
+    cfeatures = convolutional_features(inputs, FILTERS, FILTER_SIZE)
 
-cflat = Reshape((FILTERS, IMAGE_SIZE * N_DIGITS * IMAGE_SIZE))(cfeatures)
+    cflat = Reshape((FILTERS, IMAGE_SIZE * N_DIGITS * IMAGE_SIZE))(cfeatures)
 
-# Repeat
-# import IPython
-# IPython.embed()
+    # Repeat
+    # import IPython
+    # IPython.embed()
 
-output = Attention(10)(cflat)
+    output = Attention(10, return_sequences=True)(cflat)
+
+    model = Model(input=inputs, output=output)
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    return model
+
+if __name__ == "__main__":
+    model = model()
+    model.summary()
