7244110c339f3d1e122933193612bcd1ad5a91c3
Jacques Kvam <jwkvam@gmail.com>
2016-11-19 16:41:47 -0800

    compiling and training

--- network.py
+++ network.py
@@ -3,7 +3,7 @@
 
 
 from keras.models import Model
-from keras.layers import Input, Reshape
+from keras.layers import Input, Reshape, LSTM, RepeatVector, Dense
 from keras.layers.convolutional import Convolution2D
 from attention import Attention
 
@@ -14,6 +14,10 @@ N_DIGITS = 3
 def convolutional_features(inp, filters, size):
     conv = Convolution2D(filters, size, size, border_mode='same')(inp)
     conv = Convolution2D(filters, size, size, border_mode='same')(conv)
+    conv = Convolution2D(filters, 2, 2, border_mode='same', subsample=(2, 2))(conv)
+    conv = Convolution2D(filters, size, size, border_mode='same')(conv)
+    conv = Convolution2D(filters, size, size, border_mode='same')(conv)
+    conv = Convolution2D(filters, 2, 2, border_mode='same', subsample=(2, 2))(conv)
     conv = Convolution2D(filters, size, size, border_mode='same')(conv)
     conv = Convolution2D(filters, size, size, border_mode='same')(conv)
     return conv
@@ -28,14 +32,18 @@ def model():
     inputs = Input(shape=(1, IMAGE_SIZE, N_DIGITS * IMAGE_SIZE))
     cfeatures = convolutional_features(inputs, FILTERS, FILTER_SIZE)
 
-    cflat = Reshape((FILTERS, IMAGE_SIZE * N_DIGITS * IMAGE_SIZE))(cfeatures)
+    cfeatures = Reshape((FILTERS * IMAGE_SIZE * N_DIGITS * IMAGE_SIZE // 16, ))(cfeatures)
 
-    output = Attention(10, return_sequences=True)(cflat)
+    cfeatures = RepeatVector(3)(cfeatures)
+
+    output = LSTM(10, return_sequences=True)(cfeatures)
+
+    # output = Dense(10)(output)
 
     model = Model(input=inputs, output=output)
+    model.summary()
     model.compile(optimizer='adam', loss='categorical_crossentropy')
     return model
 
 if __name__ == "__main__":
     model = model()
-    model.summary()
--- train.py
+++ train.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import networks
+import network
 import joblib
+import numpy as np
 
-model = networks.model()
+model = network.model()
 
 
 features = joblib.load('features.jbl')
-labels = joblib.load('labels.jbl')
+labels = joblib.load('labels.jbl').astype(int)
 
 features = features.reshape(features.shape[0], 1,
                             features.shape[1],
                             features.shape[2])
 
-model.summary()
-print('fitting')
-model.fit(features, labels)
+print('feature shape = {}'.format(features.shape))
+print('labels shape = {}'.format(labels.shape))
+
+exp_labels = np.zeros((labels.shape[0], 10, 3))
+exp_labels[np.arange(exp_labels.shape[0])[:, None], labels, np.arange(3)[None, :]] = 1
+
+exp_labels = exp_labels.transpose((0, 2, 1))
+
+print(features.shape)
+print(exp_labels.shape)
+#
+model.fit(features, exp_labels, batch_size=2, verbose=True)
