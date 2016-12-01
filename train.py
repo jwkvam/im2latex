3fdd9ad7d548288aef16673af5c007cece284b69
Jacques Kvam <jwkvam@gmail.com>
2016-11-30 23:05:23 -0800

    nothing

--- train.py
+++ train.py
@@ -20,7 +20,6 @@ print('labels shape = {}'.format(labels.shape))
 
 exp_labels = np.zeros((labels.shape[0], 10, 3))
 exp_labels[np.arange(exp_labels.shape[0])[:, None], labels, np.arange(3)[None, :]] = 1
-
 exp_labels = exp_labels.transpose((0, 2, 1))
 
 print(features.shape)
